cd3fada7d284979bfdeeded160ddb9fdfe217c31
Vicente Santacoloma <vicente.santacoloma@justeattakeaway.com>
2021-10-21 09:03:42 +0100

    Add project

---
+++ TechPath/TechPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechPathApp.swift
+//  TechPath
+//
+//  Created by Vicente Santacoloma on 21/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct TechPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
